a330e019564ecf985fc96bec2b0efda32f59cd1e
janjan <97529177+zxc8661@users.noreply.github.com>
2025-01-02 10:58:53 +0900

    post 3, make sample data

--- rest/src/main/java/com/ll/rest/domain/post/post/service/repository/PostService.java
+++ rest/src/main/java/com/ll/rest/domain/post/post/service/repository/PostService.java
@@ -7,9 +7,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
 
+    public long count() {
+        return postRepository.count();
+    }
+
+    public Post write(String title, String content) {
+        Post post = Post.builder().
+                title(title).
+                content(content).
+                build();
+        return this.postRepository.save(post);
+        //insert into post(created_at,modified_at,content,title) values(?,?,?,?);
+    }
 }
--- rest/src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ rest/src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -1,5 +1,8 @@
 package com.ll.rest.global.baseInit;
 
+import com.ll.rest.domain.post.post.entity.Post;
+import com.ll.rest.domain.post.post.repository.PostRepository;
+import com.ll.rest.domain.post.post.service.repository.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -10,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
+    private final PostService postService;
     @Autowired
     @Lazy
     private BaseInitData self;
@@ -21,5 +25,12 @@ public class BaseInitData {
     }
     @Transactional
     public void work1() {
+        if(postService.count()>0)return;
+
+        Post post1 = postService.write("죽구 하실 분?", "14시 까지 22명을 모아야 함니다.");
+        Post post2 = postService.write("배구 하실 분?", "15시 까지 12명을 모아야 함니다.");
+        Post post3 =postService.write("농구 하실 분?", "16시 까지 10명을 모아야 함니다.");
+
     }
+
 }
